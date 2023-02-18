a235a5d17693cce8c780d236fe3d0dc26c0f6a2d
Michael Wu <mw10013@gmail.com>
2023-02-18 14:13:37 -0500

    wip

--- apps/sb/app/routes/_index.tsx
+++ apps/sb/app/routes/_index.tsx
@@ -49,24 +49,23 @@ const query = `#graphql
 `;
 
 export const loader = (async ({context}: LoaderArgs) => {
-  const result = await context.storefront.query(query);
-  console.log({result});
-  return {result};
+  const data = await context.storefront.query(query);
+  return {data};
 }) satisfies LoaderFunction;
 
 export default function HomeRoute() {
   const data = useLoaderData();
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 h-screen">
+    <div className="relative isolate overflow-hidden bg-gray-900 min-h-screen">
       <img
         // src={background}
         src="https://cdn.shopify.com/s/files/1/0720/0230/6368/files/background-webp.webp?v=1676655454"
         alt=""
-        className="absolute inset-0 -z-10 h-full w-full object-cover"
+        className="absolute inset-0 -z-10 h-full w-full object-none"
       />
       <img src={logo} alt="" className="" />
       <div className="bg-gray-200 p-4">
-        <details className="outline outline-2 outline-blue-300 p-4">
+        <details open className="outline outline-2 outline-blue-300 p-4">
           <summary>JSON</summary>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </details>
