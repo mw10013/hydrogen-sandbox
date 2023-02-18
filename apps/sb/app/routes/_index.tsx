966026dc2441218576f822abd9848a5216f134b1
Michael Wu <mw10013@gmail.com>
2023-02-18 13:55:59 -0500

    wip

--- apps/sb/app/routes/_index.tsx
+++ apps/sb/app/routes/_index.tsx
@@ -1,20 +1,76 @@
 /* eslint-disable hydrogen/prefer-image-component */
+import {useLoaderData} from '@remix-run/react';
+import {LoaderArgs, LoaderFunction} from '@shopify/remix-oxygen';
 import background from '~/assets/background-webp.webp';
-import logo from `~/assets/logo-webp.webp`
+import logo from '~/assets/logo-webp.webp';
+
+// https://cdn.shopify.com/s/files/1/0720/0230/6368/files/background.webp?v=1676653150
+
+const query = `#graphql
+  query Layout {
+    metaobjects(type: "nav_bar", first: 5) {
+      nodes {
+        __typename
+        handle
+        type
+        fields {
+          __typename
+          type
+          key
+          value
+          references(first: 10) {
+            nodes {
+              __typename
+              ... on Metaobject {
+                handle
+                type
+                fields {
+                  __typename
+                  type
+                  key
+                  value
+                  reference {
+                    __typename
+                    ... on MediaImage {
+                      previewImage {
+                        __typename
+                        url
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const loader = (async ({context}: LoaderArgs) => {
+  const result = await context.storefront.query(query);
+  console.log({result});
+  return {result};
+}) satisfies LoaderFunction;
 
 export default function HomeRoute() {
+  const data = useLoaderData();
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 h-screen">
       <img
-        // src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-        src={background}
+        // src={background}
+        src="https://cdn.shopify.com/s/files/1/0720/0230/6368/files/background-webp.webp?v=1676655454"
         alt=""
         className="absolute inset-0 -z-10 h-full w-full object-cover"
       />
-      <img
-        src={logo}
-        alt=""
-        className="" />
+      <img src={logo} alt="" className="" />
+      <div className="bg-gray-200 p-4">
+        <details className="outline outline-2 outline-blue-300 p-4">
+          <summary>JSON</summary>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </details>
+      </div>
     </div>
   );
 }
