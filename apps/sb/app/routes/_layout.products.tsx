40e8a4cc577f2d14e2901354d7e01155b8d236c7
Michael Wu <mw10013@gmail.com>
2023-02-20 12:50:13 -0500

    .

---
+++ apps/sb/app/routes/_layout.products.tsx
@@ -0,0 +1,9 @@
+export default function Route() {
+  return (
+    <div className="px-4 sm:px-6 lg:px-8 py-8">
+      <div className="border-4 border-black mx-auto max-w-6xl bg-black/40">
+        <div className="text-white text-3xl pt-4 px-8">Our Boxes</div>
+      </div>
+    </div>
+  );
+}
