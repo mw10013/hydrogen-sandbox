fda354870a7b3575286dca9ad20298e2b1da46c8
Michael Wu <mw10013@gmail.com>
2023-02-20 13:52:59 -0500

    .

--- apps/sb/app/routes/_layout.products.tsx
+++ apps/sb/app/routes/_layout.products.tsx
@@ -37,7 +37,7 @@ export default function Route() {
   return (
     <div className="px-4 sm:px-6 lg:px-8 py-8">
       <div className="border-4 border-black mx-auto max-w-6xl bg-black/40">
-        <div className="text-white text-3xl pt-4 px-8">Our Boxes</div>
+        <div className="text-3xl pt-4 px-8 text-white ">Our Boxes</div>
         <div>
           {data.products.nodes.map((i: any) => {
             return (
