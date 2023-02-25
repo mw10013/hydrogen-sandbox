4a4b3b542d13e97a21ed7caa2c82a98c0100049d
Michael Wu <mw10013@gmail.com>
2023-02-24 22:52:22 -0500

    .

--- apps/sb/app/routes/_layout.tsx
+++ apps/sb/app/routes/_layout.tsx
@@ -1,9 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable hydrogen/prefer-image-component */
 import {Link, Outlet, useLoaderData} from '@remix-run/react';
-import {LoaderArgs, LoaderFunction} from '@shopify/remix-oxygen';
+import {
+  AppLoadContext,
+  LoaderArgs,
+  LoaderFunction,
+} from '@shopify/remix-oxygen';
 import {Image} from '@shopify/hydrogen-react';
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  QuestionMarkCircleIcon,
+  ShoppingBagIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline';
+import invariant from 'tiny-invariant';
+import {Storefront} from '@shopify/hydrogen';
+import {Cart} from '@shopify/hydrogen/storefront-api-types';
 
-const query = `#graphql
+const LAYOUT_QUERY = `#graphql
 query Layout {
   layout: metaobject(handle: {handle: "main-layout", type: "layout"}) {
     fields {
@@ -50,8 +65,8 @@ fragment mediaImageFields on MediaImage {
 }
 `;
 
-export const loader = (async ({context}: LoaderArgs) => {
-  const result = (await context.storefront.query(query)) as any;
+async function getLayoutData({storefront}: AppLoadContext) {
+  const result = (await storefront.query(LAYOUT_QUERY)) as any;
   const data: any = {};
   for (const field of result.layout.fields) {
     switch (field.key) {
@@ -60,7 +75,7 @@ export const loader = (async ({context}: LoaderArgs) => {
         data[field.key] = field.reference.image;
         break;
       case 'nav_links':
-        data[field.key] = field.references.nodes.map((i) => {
+        data[field.key] = field.references.nodes.map((i: any) => {
           const o: any = {};
           for (const f of i.fields) {
             switch (f.type) {
@@ -78,12 +93,160 @@ export const loader = (async ({context}: LoaderArgs) => {
     }
   }
   return data;
+}
+
+const CART_QUERY = `#graphql
+  query CartQuery($cartId: ID!, $country: CountryCode, $language: LanguageCode)
+    @inContext(country: $country, language: $language) {
+    cart(id: $cartId) {
+      ...CartFragment
+    }
+  }
+
+  fragment CartFragment on Cart {
+    id
+    checkoutUrl
+    totalQuantity
+    buyerIdentity {
+      countryCode
+      customer {
+        id
+        email
+        firstName
+        lastName
+        displayName
+      }
+      email
+      phone
+    }
+    lines(first: 100) {
+      edges {
+        node {
+          id
+          quantity
+          attributes {
+            key
+            value
+          }
+          cost {
+            totalAmount {
+              amount
+              currencyCode
+            }
+            amountPerQuantity {
+              amount
+              currencyCode
+            }
+            compareAtAmountPerQuantity {
+              amount
+              currencyCode
+            }
+          }
+          merchandise {
+            ... on ProductVariant {
+              id
+              availableForSale
+              compareAtPrice {
+                ...MoneyFragment
+              }
+              price {
+                ...MoneyFragment
+              }
+              requiresShipping
+              title
+              image {
+                ...ImageFragment
+              }
+              product {
+                handle
+                title
+                id
+              }
+              selectedOptions {
+                name
+                value
+              }
+            }
+          }
+        }
+      }
+    }
+    cost {
+      subtotalAmount {
+        ...MoneyFragment
+      }
+      totalAmount {
+        ...MoneyFragment
+      }
+      totalDutyAmount {
+        ...MoneyFragment
+      }
+      totalTaxAmount {
+        ...MoneyFragment
+      }
+    }
+    note
+    attributes {
+      key
+      value
+    }
+    discountCodes {
+      code
+    }
+  }
+
+  fragment MoneyFragment on MoneyV2 {
+    currencyCode
+    amount
+  }
+
+  fragment ImageFragment on Image {
+    id
+    url
+    altText
+    width
+    height
+  }
+`;
+
+async function getCart({storefront}: AppLoadContext, cartId: string) {
+  invariant(storefront, 'missing storefront client in cart query');
+
+  const {cart} = await storefront.query<{cart?: Cart}>(CART_QUERY, {
+    variables: {
+      cartId,
+      country: storefront.i18n.country,
+      language: storefront.i18n.language,
+    },
+    cache: storefront.CacheNone(),
+  });
+
+  return cart;
+}
+
+export const loader = (async ({context}: LoaderArgs) => {
+  return await getLayoutData(context);
 }) satisfies LoaderFunction;
 
 function Header() {
   const data = useLoaderData();
   return (
     <div className="px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between">
+        <div></div>
+        <div className="bg-black opacity-80 mt-8 p-2 rounded-md shadow">
+          <a href="#" className="group -m-2 flex items-center p-2">
+            <ShoppingBagIcon
+              className="h-6 w-6 flex-shrink-0 text-gray-200 group-hover:text-gray-500"
+              aria-hidden="true"
+            />
+            <span className="ml-2 text-sm font-medium text-gray-100 group-hover:text-gray-800">
+              0
+            </span>
+            <span className="sr-only">items in cart, view bag</span>
+          </a>
+        </div>
+      </div>
       <div className="lg:flex lg:justify-between mt-4">
         <Link to="/">
           {/* <Image data={data.logo} loaderOptions={{crop: 'left', width: 820}} /> */}
