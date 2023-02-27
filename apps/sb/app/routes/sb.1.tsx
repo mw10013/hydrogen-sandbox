11d0d5454dd0c1368d7904d153f44e20b227af6e
Michael Wu <mw10013@gmail.com>
2023-02-27 13:31:26 -0500

    .

--- apps/sb/app/routes/sb.1.tsx
+++ apps/sb/app/routes/sb.1.tsx
@@ -4,11 +4,17 @@ import {Await, useLoaderData} from '@remix-run/react';
 import {defer, LoaderFunction} from '@shopify/remix-oxygen';
 import {Suspense} from 'react';
 
+async function getDeferredValue() {
+  await new Promise((resolve) => setTimeout(resolve, 9000));
+  return 'deferredValue';
+}
+
 export const loader = (async () => {
   const immediateValue = 'immediateValue';
-  const deferredValue = new Promise((resolve) => {
-    setTimeout(() => resolve('deferredValue'), 3000);
-  });
+  // const deferredValue = new Promise((resolve) => {
+  //   setTimeout(() => resolve('deferredValue'), 3000);
+  // });
+  const deferredValue = getDeferredValue();
   return defer({
     immediateValue,
     deferredValue,
